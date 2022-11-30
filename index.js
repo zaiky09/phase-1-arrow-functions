27c79bdd2d9bc5febcbe7fe8cf884f4cef6aed59
zaiky09 <zamilmozamil@outlook.com>
2022-11-30 15:12:06 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,11 @@
+//Dividing 2000/100
+
+const divide = (num1 = 2000, num2 = 100) => num1 / num2;
+
+//Square
+
+const square = number => number * number
+
+//Add
+
+const add = (number1 , number2) => number1 + number2
